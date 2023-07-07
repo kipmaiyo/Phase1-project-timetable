c99084539e3c6fac8d12263c38332f51d08db01a
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-07-07 08:58:48 +0300

    Week-3 Code Chellenge

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// JavaScript for index.html
+
 document.addEventListener('DOMContentLoaded', fetchTimetable);
 
 async function fetchTimetable() {
@@ -13,18 +13,103 @@ async function fetchTimetable() {
 
 function populateTimetable(timetable) {
   const table = document.getElementById('timetable');
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
+  // Create table headers
+  const headerRow = document.createElement('tr');
+  const timeHeader = document.createElement('th');
+  timeHeader.textContent = 'Time';
+  headerRow.appendChild(timeHeader);
+  days.forEach(day => {
+    const dayHeader = document.createElement('th');
+    dayHeader.textContent = day;
+    headerRow.appendChild(dayHeader);
+  });
+  table.appendChild(headerRow);
+
+  // Populate table with timetable data
   timetable.forEach(entry => {
     const row = document.createElement('tr');
-    Object.values(entry).forEach(value => {
-      const cell = document.createElement('td');
+    const timeCell = document.createElement('td');
+    timeCell.textContent = entry.time;
+    row.appendChild(timeCell);
+
+    days.forEach(day => {
+      const dayCell = document.createElement('td');
       const input = document.createElement('input');
       input.type = 'text';
-      input.value = value;
-      cell.appendChild(input);
-      row.appendChild(cell);
+      input.value = entry[day.toLowerCase()];
+      dayCell.appendChild(input);
+      row.appendChild(dayCell);
     });
+
     table.appendChild(row);
   });
 }
+
+function saveData() {
+  const formContainer = document.getElementById('form-container');
+  formContainer.innerHTML = '<h2>This is your schedule for the week</h2>';
+
+  const timetable = document.getElementById('timetable');
+  const rows = timetable.querySelectorAll('tr');
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  // Create table for form data
+  const formDataTable = document.createElement('table');
+  formDataTable.className = 'form-data-table';
+
+  // Create table headers for form data table
+  const headerRow = document.createElement('tr');
+  const timeHeader = document.createElement('th');
+  timeHeader.textContent = 'Time';
+  headerRow.appendChild(timeHeader);
+  days.forEach(day => {
+    const dayHeader = document.createElement('th');
+    dayHeader.textContent = day;
+    headerRow.appendChild(dayHeader);
+  });
+  formDataTable.appendChild(headerRow);
+
+  // Populate form data table with entered values
+  rows.forEach((row, index) => {
+    const cells = row.querySelectorAll('td');
+    const rowData = [];
+
+    cells.forEach((cell, cellIndex) => {
+      if (cellIndex === 0) {
+        const time = cell.textContent;
+        rowData.push(time);
+      } else {
+        const input = cell.querySelector('input');
+        rowData.push(input.value);
+      }
+    });
+
+    const formDataRow = document.createElement('tr');
+    rowData.forEach(value => {
+      const dataCell = document.createElement('td');
+      dataCell.textContent = value;
+      formDataRow.appendChild(dataCell);
+    });
+
+    formDataTable.appendChild(formDataRow);
+  });
+
+  formContainer.appendChild(formDataTable);
+}
+
+const saveButton = document.getElementById('save-button');
+saveButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  saveData();
+});
+
+
+
+
+
+
+
+
 
